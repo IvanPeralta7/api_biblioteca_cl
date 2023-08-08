9dc65d1818b41812d46c0ed7bae31216c8ff1ab1
Ivan peralta <ivanperalta_cba2013@live.com>
2023-08-08 18:53:34 -0300

    Nuevo Endpoint Usuarios

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const express = require("express");
-
 const { auth } = require("express-oauth2-jwt-bearer");
 const errorHandler = require("./middlewares/errorHandler");
-
-
 require('dotenv').config();
+
 
 // Configuracion Middleware con el Servidor de Autorización 
 const autenticacion = auth({
@@ -20,8 +18,13 @@ app.use(express.json());
 // Importamos el Router de Libros
 const librosRouter = require("./routes/libros");
 
+// Importamos el Router de Clientes
+const clientesRouter = require("./routes/clientes");
+
 //Configuramos el middleware de autenticacion
 app.use("/api/libros", autenticacion,  librosRouter);
+
+app.use("/api/clientes", autenticacion, clientesRouter);
 
 app.use(errorHandler);
 
